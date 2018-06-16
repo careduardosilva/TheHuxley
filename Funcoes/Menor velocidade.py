bf9e1be5aad58e3d5178ddaeae8eedf089a67813
careduardosilva <car.eduardosilva@hotmail.com>
2018-06-16 00:49:07 -0300

    Update Menor velocidade.py

--- Funcoes/Menor velocidade.py
+++ Funcoes/Menor velocidade.py
@@ -1,14 +1,16 @@
-def velkmh(v0,a,t): #Funcao que calcula velocidade (v0,a,t) sao os argumentos
-    return (v0 + a*t)*3.6 #Retornamos o valor dessa operacao ai
-v = 0
+def velkmh(v0,a,t): #Definicao da funcao velkmh
+    return (v0 + a*t)*3.6
+v = 0 #Inicializacao das variavei
 menorab = 0
-for i in range(0,3): #Para nao criarmos 9 variaveis, podemos usar um laço for para repetir as entradas.
-    vaux = v
-    v0 = float(input()) #Nao, necessariamente, precisamos colocar o nome da variavel igual ao do argumento, pode ser qualquer nome.
-    a = float(input()) #Apenas coloquei o mesmo nome, a intuito, de facilitar a compreensao
+for i in range(0,3): #Iremos iterar 3 vezes, isso a fim de evitar criarmos 9 variaveis
+    vaux = v #Em vaux, guardamos sempre os valores anteriores de v
+    menoraux = menorVAUXeV #Essa variavel vai ser responsavel por guardar os menores valores entre v e vaux. Com o intuito, de usarmos ele no final, para sabermos o menor dos 3.
+    v0 = float(input())
+    a = float(input())
     t = float(input())
-    v = velkmh(v0,a,t) #Retornar significa que a funcao velkmh, quando "INVOCADA" vai retornar o valor da operacao do return.
-    menorab = (vaux+v-abs(vaux-v))/2 #Podemos obter os menores valores, pela formula do exercicio "O Maior", a deducao nao me compete.
-print("%.1f"%menorab)
-
-#CODIGO EM CONSTRUCAO
+    v = velkmh(v0,a,t) #Podemos obter os menores valores, pela formula do exercicio anterior
+    menorVAUXeV = (v+vaux-abs(v-vaux))/2 #Atraves dessa operacao, obteremos os menor valor entre 2. Essa formula eh semelhante a do exercicio anterior chamado "O Maior" favor consultar.
+    #A sua respectiva deducao nao me compete. Se vire
+menordos3 = (v+menoraux-abs(v-menoraux))/2 #Como menoraux nos dar o menor entre 2 valores, nesse caso sera entre os 2 primeiros,entao, basta fazermos a operacao de obter o menor, novamente.
+print("%.1f"%menorabc)
+#O Exemplo de caso eh dispendioso, por isso nao irei demonstrar aqui. Sugiro que visitem o site pythontutor.com
