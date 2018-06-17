ccec636124d15e7e3e7d44e634abb35ebff300b0
careduardosilva <car.eduardosilva@hotmail.com>
2018-06-17 00:51:10 -0300

    Update Apagando e Ganhando.py

--- Apagando e Ganhando.py
+++ Apagando e Ganhando.py
@@ -3,14 +3,17 @@ while(True):
     n = int(string[0])
     d = int(string[1])
     if(n != 0 and d != 0):
+        j = 0
         i = 0
         number = int(input())
         number_list = list(str(number))
         stack = []
         result = ''
         for numbers in number_list:
-            numbers = int(numbers)
-            stack.append(numbers)
+            if(j != n):
+                numbers = int(numbers)
+                stack.append(numbers)
+                j += 1
         while i < d:
             stack.remove(min(stack))
             i += 1
